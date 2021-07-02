296b590857ad5a0ee7be0f60c06139fd631b8a5b
Ryobia <ryobia36@gmail.com>
2021-07-02 14:06:56 -0600

    added savannahs sweets

--- src/components/Portfolio/index.js
+++ src/components/Portfolio/index.js
@@ -6,6 +6,7 @@ import runBuddy from "../../assets/work/run-buddy.png";
 import budget from "../../assets/work/budget.png";
 import weather from "../../assets/work/weather.png";
 import schedule from "../../assets/work/schedule.png";
+import sweets from '../../assets/work/sweets.png'
 
 function Portfolio() {
   return (
@@ -14,11 +15,43 @@ function Portfolio() {
       <div className="work-div">
         <div className="container">
           <a
+            href="https://dudley-doright-93373.herokuapp.com/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <img
+              className="work-img"
+              src={sweets}
+              height="300px"
+              alt="ghostwriter"
+            />
+          </a>
+          <h3 className="centered">
+            <a
+              target="_blank"
+              rel="noreferrer"
+              href="https://github.com/Ryobia/savannahs-sweets"
+            >
+              <img src={github} height="35px" alt="github-icon" />
+            </a>
+          </h3>
+
+          <p className="centered-text">
+            Here you can order from a local bakery! - GraphQL/ React/ MongoDB/ Node.js
+          </p>
+        </div>
+        <div className="container">
+          <a
             href="https://desolate-harbor-29412.herokuapp.com/"
             target="_blank"
             rel="noreferrer"
           >
-            <img className="work-img" src={ghost} height="300px" alt="ghostwriter" />
+            <img
+              className="work-img"
+              src={ghost}
+              height="300px"
+              alt="ghostwriter"
+            />
           </a>
           <h3 className="centered">
             {/* Ghostwriter{" "} */}
@@ -31,7 +64,10 @@ function Portfolio() {
             </a>
           </h3>
 
-          <p className="centered-text">A site where you can tell stories - Node.js/ MySQL/ Sequelize/ Handlebars</p>
+          <p className="centered-text">
+            A site where you can tell stories - Node.js/ MySQL/ Sequelize/
+            Handlebars
+          </p>
         </div>
 
         <div className="container">
@@ -40,7 +76,12 @@ function Portfolio() {
             target="_blank"
             rel="noreferrer"
           >
-            <img className="work-img" src={nightIn} height="300px" alt="a-night-in" />
+            <img
+              className="work-img"
+              src={nightIn}
+              height="300px"
+              alt="a-night-in"
+            />
           </a>
           <h3 className="centered">
             {/* A Night In{" "} */}
@@ -52,7 +93,10 @@ function Portfolio() {
               <img src={github} height="35px" alt="github-icon" />
             </a>
           </h3>
-          <p className="centered-text">A site to help you plan your next Night-In - Web API's/ localstorage/ DOM manipulation</p>
+          <p className="centered-text">
+            A site to help you plan your next Night-In - Web API's/
+            localstorage/ DOM manipulation
+          </p>
         </div>
 
         <div className="container">
@@ -61,7 +105,12 @@ function Portfolio() {
             target="_blank"
             rel="noreferrer"
           >
-            <img className="work-img" src={runBuddy} height="300px" alt="runbuddy" />
+            <img
+              className="work-img"
+              src={runBuddy}
+              height="300px"
+              alt="runbuddy"
+            />
           </a>
           <h3 className="centered">
             {/* Run Buddy{" "} */}
@@ -73,7 +122,9 @@ function Portfolio() {
               <img src={github} height="35px" alt="github-icon" />
             </a>
           </h3>
-          <p className="centered-text">A site built for a running/phyical fitness website - HTML/CSS</p>
+          <p className="centered-text">
+            A site built for a running/phyical fitness website - HTML/CSS
+          </p>
         </div>
 
         <div className="container">
@@ -82,7 +133,12 @@ function Portfolio() {
             target="_blank"
             rel="noreferrer"
           >
-            <img className="work-img" src={budget} height="300px" alt="team-profile-generator" />
+            <img
+              className="work-img"
+              src={budget}
+              height="300px"
+              alt="team-profile-generator"
+            />
           </a>
           <h3 className="centered">
             {/* PWA - Budget Tracker{" "} */}
@@ -94,7 +150,10 @@ function Portfolio() {
               <img src={github} height="35px" alt="github-icon" />
             </a>
           </h3>
-          <p className="centered-text">A PWA where you can track your budget - Manifest.json/ indexedDB/ service worker</p>
+          <p className="centered-text">
+            A PWA where you can track your budget - Manifest.json/ indexedDB/
+            service worker
+          </p>
         </div>
 
         <div className="container">
@@ -103,7 +162,12 @@ function Portfolio() {
             target="_blank"
             rel="noreferrer"
           >
-            <img className="work-img" src={weather} height="300px" alt="weather-dashboard" />
+            <img
+              className="work-img"
+              src={weather}
+              height="300px"
+              alt="weather-dashboard"
+            />
           </a>
           <h3 className="centered">
             {/* Weather Dashboard{" "} */}
@@ -115,7 +179,10 @@ function Portfolio() {
               <img src={github} height="35px" alt="github-icon" />
             </a>
           </h3>
-          <p className="centered-text">Find the weather for anywhere in the world! - openweatherAPI/ DOM Manipulation/ JQuery</p>
+          <p className="centered-text">
+            Find the weather for anywhere in the world! - openweatherAPI/ DOM
+            Manipulation/ JQuery
+          </p>
         </div>
 
         <div className="container">
@@ -124,7 +191,12 @@ function Portfolio() {
             target="_blank"
             rel="noreferrer"
           >
-            <img className="work-img" src={schedule} height="300px" alt="day-scheduler" />
+            <img
+              className="work-img"
+              src={schedule}
+              height="300px"
+              alt="day-scheduler"
+            />
           </a>
           <h3 className="centered">
             {/* Day Scheduler{" "} */}
@@ -136,7 +208,9 @@ function Portfolio() {
               <img src={github} height="35px" alt="github-icon" />
             </a>
           </h3>
-          <p className="centered-text">Schedule your day a day at a time - Moment.js/ JQuery/ Bootstrap</p>
+          <p className="centered-text">
+            Schedule your day a day at a time - Moment.js/ JQuery/ Bootstrap
+          </p>
         </div>
       </div>
     </div>
